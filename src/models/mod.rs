ff227d71546e9df23bfb577ab682cac35fe59c65
Nalysius <anthony.bocci+git@protonmail.com>
2023-07-10 20:39:59 +0200

    Cleanup in models
    The technology and the port are not needed as structures.
    The regex in the technology will be replaced by the notion of
    checkers, structures created to check for one technology. They
    will contain the regex and do the job. The technology name
    has been moved in the finding, and the version in the finding
    is now optional.

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,58 +1,47 @@
 //! In this module are declared the entities manipulated by this program
 
-use self::technologies::Technology;
-
-pub mod technologies;
-
-/// Represents which protocol is used between TCP and UDP
+/// Represents the type of scan
 #[derive(Clone, PartialEq)]
-pub enum TransportLayerProtocol {
+pub enum ScanType {
     /// Protocol TCP
     Tcp,
     /// Protocol UDP
     Udp,
+    /// Protocol HTTP
+    Http,
 }
 
-/// Represents a port
-#[derive(Clone, PartialEq)]
-pub struct Port {
-    /// The number of the port
-    pub number: u16,
-    /// Which protocol to use to connect to the port
-    pub layer4_protocol: TransportLayerProtocol,
-}
-
-impl Port {
-    /// Creates a new TCP port
-    pub fn new_tcp(number: u16) -> Self {
-        Port {
-            number: number,
-            layer4_protocol: TransportLayerProtocol::Tcp,
-        }
-    }
-
-    /// Create a new UDP port
-    pub fn new_udp(number: u16) -> Self {
-        Port {
-            number,
-            layer4_protocol: TransportLayerProtocol::Udp,
-        }
-    }
-}
 
 /// Represents an asset to scan
 pub struct Asset {
-    /// The IPv4 address or hostname of the asset
+    /// The IP address or hostname of the asset
     /// A target can be scanned either by its IP address or its hostname
     pub ip_hostname: String,
     /// The list of ports to scan on the asset
-    pub ports_to_scan: Vec<Port>,
+    pub ports_to_scan: Vec<u16>,
 }
 
 /// Represents a finding of a technology running on an asset
 pub struct Finding {
     /// The technology found
-    pub technology: Technology,
+    pub technology: String,
     /// The version of the technology
-    pub version: String,
+    /// Optional since it can be unknown
+    pub version: Option<String>,
+    /// The evidence of the finding
+    pub evidence: String,
+    /// The text for the evidence
+    pub evidence_text: String,
+}
+
+impl Finding {
+    /// Creates a new finding
+    pub fn new(technology: &str, version: Option<&str>, evidence: &str, evidence_text: &str) -> Self {
+        Finding {
+            technology: technology.to_string(),
+            version: version.map(|f| f.to_string()),
+            evidence: evidence.to_string(),
+            evidence_text: evidence_text.to_string()
+        }
+    }
 }
